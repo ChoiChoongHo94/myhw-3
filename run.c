b5a09d8600de8353d6787066dbc6e5c2ab5cd506
ChoiCHoongHO94 <cndgh49@ajou.ac.kr>
2017-12-16 19:27:45 +0900

    complete!

--- run.c
+++ run.c
@@ -1,6 +1,7 @@
 #include <sys/types.h>
 #include <limits.h>
 #include <stdlib.h>
+#include <sys/resource.h>
 
 #include "run.h"
 #include "util.h"
@@ -40,30 +41,18 @@ p_meta find_meta(p_meta *last, size_t size) {
     case BEST_FIT:
     {
       //BEST_FIT CODE
-	int count=0;
-        p_meta temp;
-        size_t min=9999999999;
-        while(index != NULL){
-                if(index->free == 1){
-                        if(index->size >= size){
-                                if(index->size < min){
-                                        temp = index;
-                                        min = index->size;
-                                        count++;
-                                }
-                                break;
-                        }
-                        else{
-                                index = index->next;
-                        }
+        p_meta temp=NULL;
+        size_t min=9999;
+        while(index){
+                if(index->free == 1 && index->size >= size && index->size < min){
+                	temp = index;
+                        min = index->size;
                 }
-                else // free == 0
-                        index = index->next;
+                index = index->next;
         }
 
-        //not found
-        if(count ==0)
-                result = NULL;
+    
+        result = temp; 
 
     }
     break;
@@ -71,30 +60,17 @@ p_meta find_meta(p_meta *last, size_t size) {
     case WORST_FIT:
     {
       //WORST_FIT CODE
-	int count=0;
-        p_meta temp;
+        p_meta temp=NULL;
         size_t max=0;
-        while(index != NULL){
-                if(index->free == 1){
-                        if(index->size >= size){
-                                if(index->size > max){
-                                        temp = index;
-                                        max = index->size;
-                                        count++;
-                                }
-                                break;
-                        }
-                        else{
-                                index = index->next;
-                        }
-                }
-                else // free == 0
-                        index = index->next;
+        while(index){
+                if(index->free == 1 && index->size >=size && index->size > max){     
+                        temp = index;
+                       	max = index->size;
+                }    
+                index = index->next;
         }
 
-        //not found
-        if(count ==0)
-                result = NULL;
+        result = temp;
 
     }
     break;
@@ -102,6 +78,8 @@ p_meta find_meta(p_meta *last, size_t size) {
   }
   return result;
 }
+
+struct rlimit templimit;
 
 void *m_malloc(size_t size) {
 
@@ -116,6 +94,12 @@ void *m_malloc(size_t size) {
                 p_meta create = (p_meta)malloc(META_SIZE+size);
 
 		if(create == NULL){//malloc fail
+			getrlimit(RLIMIT_AS, &templimit);
+			if(sbrk(0) + size+META_SIZE > templimit.rlim_max){
+				printf("############\n");
+				return NULL;
+			}
+			
                         if(brk(size+META_SIZE) == -1)//brk fail
                                  return NULL;
                         create = (p_meta)malloc(META_SIZE+size);
@@ -145,9 +129,9 @@ void *m_malloc(size_t size) {
 			p_meta tempprev= temp->prev;
 			p_meta tempnext= temp->next;
 			free(temp);
-			tempprev->next = (p_meta)malloc(META_SIZE+ size*sizeof(char));
+			tempprev->next = (p_meta)malloc(META_SIZE+ size);
 			temp = tempprev->next;
-			temp->next = (p_meta)malloc(META_SIZE + tempsize*sizeof(char));
+			temp->next = (p_meta)malloc(META_SIZE + tempsize);
 	
 			temp->size = size;
 			temp->next->size = tempsize;
@@ -171,7 +155,8 @@ void m_free(void *ptr) {
 	
         p_meta temp = ptr- META_SIZE;
 	
-        while(1){//coalesce
+	
+        //while(1){//coalesce
                 if(temp->next && temp->next->free ==1){//next
                         int tempsize = temp->size + temp->next->size+ META_SIZE;
 			
@@ -179,8 +164,7 @@ void m_free(void *ptr) {
 			p_meta tempnext= temp->next->next;
 			free(temp->next);
 			free(temp);
-			
-			
+				
 			temp = (p_meta)malloc(META_SIZE + tempsize);
 			temp->size = tempsize;
 			temp->free = 1;
@@ -198,7 +182,8 @@ void m_free(void *ptr) {
 				tempnext->prev = temp;
 
                 }
-                else if(temp->prev && temp->prev->free ==1){//prev
+
+                if(temp->prev && temp->prev->free ==1){//prev
                         int tempsize = temp->size+temp->prev->size+META_SIZE;
 			p_meta tempprev = temp->prev->prev;
 			p_meta tempnext = temp->next;
@@ -221,17 +206,18 @@ void m_free(void *ptr) {
 			else
 				tempnext->prev = temp;
                 }
-                else if(temp == last){//if last
-			last = temp->prev;
-			last->next = NULL;
-			free(temp);
-			break;
-		}
-		else{
+		
+		if(temp == last){//if last
+                        last = temp->prev;
+                        last->next = NULL;
+                        free(temp);
+                        return;
+                }
+
 			temp->free =1;
-			break;
-		}
-        }
+			//break;
+		
+        //}
 		
 	return;
 }
@@ -241,6 +227,7 @@ void* m_realloc(void* ptr, size_t size)
 	p_meta original = ptr-META_SIZE;
 	p_meta temp;
 	
+	int argsize = size;
 	if(size%4 >0) size += 4-size%4;
 
 	struct metadata tempmeta;
@@ -251,33 +238,93 @@ void* m_realloc(void* ptr, size_t size)
 	//printf("@@@ tempsting: %s @@@\n", tempstring);
 	//printf("@@@ original->size: %d @@@\n", (int)original->size);
 		
-	if(size > original->size){//more size than original(new malloc);
-		temp = m_malloc(size) - META_SIZE;//new malloc address
-		memcpy(temp, &tempmeta, META_SIZE);
-		memcpy(temp->data, tempstring, original->size);
+	if(size > original->size){//more size than original(new malloc);	
+		if(original->next && size - original->size <= original->next->size){// next is free and sufficient space
+			int tempsize = original->size;
+			int tempsizenext = original->next->size;
+			p_meta tempnext = original->next->next;
+			p_meta tempprev = original->prev;
+
+			free(original->next);
+			free(original);
+			 
+			temp = (p_meta)malloc(size+META_SIZE);
+			memcpy(temp, &tempmeta, META_SIZE);
+			memcpy(temp->data, tempstring, tempsize);
+			temp->size = size;
 		
-		m_free(ptr);//free(original)
-		//printf("@@@ temp->free: %d, temp->size: %d @@@\n", temp->free, (int)temp->size);
-        	temp->free = 0;
-		//printf("@@@ temp->size: %d, size: %d @@@\n", (int)temp->size, (int)size);
-        	temp->size = size;
+			temp->next = (p_meta)malloc(tempsizenext - (size-tempsize) + META_SIZE);
+			temp->next->free =1;
+			temp->next->next = tempnext;
+			temp->next->prev = temp;
+			temp->next->size = tempsizenext -(size-tempsize);
+		
+			if(!tempprev)
+				base = temp;
+			else
+				tempprev->next = temp;
+
+			if(!tempnext)
+				last = temp->next;
+			else
+				tempnext->prev = temp->next;
+				
+			return (char*)temp->data;
+		}
+		else{// next is not free or not sufficient space
+			temp = m_malloc(size) - META_SIZE;//new malloc address
+			memcpy(temp, &tempmeta, META_SIZE);
+			memcpy(temp->data, tempstring, original->size);
+			
+			m_free(ptr);//free(original)
+			//printf("@@@ temp->free: %d, temp->size: %d @@@\n", temp->free, (int)temp->size);
+        		temp->free = 0;
+			//printf("@@@ temp->size: %d, size: %d @@@\n", (int)temp->size, (int)size);
+        		temp->size = size;
+		
+			return (char*) temp->data;
+		}
 	}
 	else{//less size than original
 		if(original->size - size > META_SIZE){//partitioning
 			int tempsize = original->size;
-			m_free(original);
-			//partition1
-			temp = m_malloc(size)-META_SIZE;
-			memcpy(temp, &tempmeta, META_SIZE);
-			memcpy(temp->data, tempstring, size);
-			temp->size = size;
+			p_meta tempprev = original->prev;
+			p_meta tempnext = original->next;
 			
+
+			//partition1
+			temp = (p_meta)malloc(size+META_SIZE);
+			memcpy(temp, &tempmeta, META_SIZE);
+			memcpy(temp->data, tempstring, argsize);
+
+			temp->size = size;
+			temp->free = 0;
+			temp->prev = tempprev;
+			
+
 			//partition2
-			temp = m_malloc(tempsize-size-META_SIZE);
-			temp->free =1;
+			temp->next = (p_meta)malloc(tempsize- size - META_SIZE);
+			temp->next->size = tempsize-size-META_SIZE;
+			temp->next->free =1;
+			temp->next->prev = temp;
+			temp->next->next = tempnext;
+			
+			if(!tempprev)
+                                base = temp;
+                        else
+                                tempprev->next = temp;
+
+                        if(!tempnext)
+                                last = temp;
+                        else
+                                tempnext->prev = temp->next;
+
+			return (char*) temp->data;
 		}
+
 		else{//no partitioning
-			memcpy(original->data, tempstring, size);
+			//memcpy(original->data, tempstring, size);
+			return (char*) original->data;
 		}
 	}
 }
